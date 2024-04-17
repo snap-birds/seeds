c28c29d72bbba7da4027233a2a96d6dcb8b09d58
Ivan Kotov <ivan@kotov.ws>
2024-04-17 14:44:43 +0200

    fixes

--- actions/notify/dist/index.js
+++ actions/notify/dist/index.js
@@ -31084,14 +31084,19 @@ const github = __nccwpck_require__(3617);
 
 const stepsErrors = new Array(
   {id: "req_generate#(failure|skipped)", error: 'HANDLE_TEMPLATE_ERROR', context: ["run_id"]},
-  {id: "req_create_pr_check#(failure|skipped)", error: 'HANDLE_NO_CHANGES_ERROR', context: ["run_id"]},
+  {id: "req_create_pr_check#(failure|skipped)", error: 'HANDLE_UNCHANGED_ERROR', context: ["run_id"]},
   {id: "req_create_pr#(failure|skipped)", error: 'HANDLE_CREATE_PR_ERROR', context: ["run_id"]},
   {id: "req_create_pr#success", error: 'HANDLE_SUCCESS', context: ["run_id", "pull-request-url"]},
 );
 
+// const inputStepsJson = '{"req_generate":{"outputs":{},"outcome":"success","conclusion":"success"},"req_create_pr":{"outputs":{},"outcome":"success","conclusion":"success"},"req_create_pr_check":{"outputs":{},"outcome":"failure","conclusion":"failure"}}';
+// const inputMessageJson = '{"foo": 1, "bar": 2}'
+
 async function main() {
   const inputSteps = new Map(Object.entries(JSON.parse(core.getInput('steps', {required: true}))));
   const inputMessage = new Map(Object.entries(JSON.parse(core.getInput('message', {required: true}))))
+  // const inputSteps = new Map(Object.entries(JSON.parse(inputStepsJson)));
+  // const inputMessage = new Map(Object.entries(JSON.parse(inputMessageJson)));
 
   let message = Object();
   let step = Object();
@@ -31100,12 +31105,14 @@ async function main() {
     let [stepError, stepErrorStatus] = stepsErrors[i].id.split('#');
 
     if (inputSteps.has(stepError)) {
-      step = inputSteps[stepError];
-      if (step.outcome.math(stepErrorStatus)) {
+      step = inputSteps.get(stepError);
+      console.log(step);
+      if (step.outcome.match(stepErrorStatus)) {
         message = {
           status: stepsErrors[i].error,
-          context: Object.fromEntries(stepError.context.map(
+          context: Object.fromEntries(stepsErrors[i].context.map(
             ctx => [ctx, (step.outputs[ctx] ?? github.context.get(ctx) ?? '')]
+            // ctx => [ctx, (step.outputs[ctx] ?? '')]
           ))
         };
         break;
